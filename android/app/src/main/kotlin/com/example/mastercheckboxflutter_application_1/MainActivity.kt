70e1c2acc7ad0dd2db5721e84580592d8bc62c70
maruf jfz <marufjfz2163@gmail.com>
2021-12-11 00:14:41 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/mastercheckboxflutter_application_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mastercheckboxflutter_application_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/checknotification.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class Checkboxnotification {
+  bool value;
+  String title;
+  Checkboxnotification({required this.title, @required this.value = false});
+}
